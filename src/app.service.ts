b40862f43dabc350e9fcda377f6e385d0ecebbb6
Suraj <suraj@jify.co>
2022-11-11 14:10:30 +0530

    removed apikeys

--- src/app.service.ts
+++ src/app.service.ts
@@ -4,7 +4,7 @@ import { v4 as uuidv4 } from 'uuid';
 import {
   MIXPANEL_API_SECRET,
   MIXPANEL_PROJECT_TOKEN,
-} from './constants/mixpanelApiKeys';
+} from '../mixpanelApiKeys';
 
 @Injectable()
 export class AppService {
